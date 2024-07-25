5a81c415f1f9db6ddc996be93b1b22af31b2d855
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-25 11:33:53 +0200

    Use template ref to access contents of p tag

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="home">Home</div>
-  <p>{{ name }}</p>
-  <p>{{ age }}</p>
+  <p ref="p">My name is {{ name }} and I'm {{ age }} years old.</p>
   <button @click="handleClick">Click Me!</button>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
   setup() {
+    const p = ref(null);
+
     console.log("setup");
+
     let name = "Jules";
     let age = 7;
 
     const handleClick = () => {
-      console.log("Clicked!");
+      console.log(p, p.value.innerText);
+      p.value.classList.add("test")
+      p.value.textContent = "Hello Vue"
     };
 
     return {
       name,
       age,
       handleClick,
+      p,
     };
   },
 };
