3ae1622f7e96f88ee1a9571a555875fa07fad419
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-07-25 11:06:30 +0200

    Add values inside of setup function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="home">Home</div>
+  <p>{{ name }}</p>
+  <p>{{ age }}</p>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+  setup() {
+    console.log("setup");
+    let name = "Jules";
+    let age = 7;
+
+    return {
+      name,
+      age,
+    };
+  },
 };
 </script>
